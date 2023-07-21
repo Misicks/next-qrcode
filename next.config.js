36a1d91f2a931bc9514c07c59e9aa430bee9995b
mysicks <alo.madri@gmail.com>
2023-07-20 22:48:25 -0600

    fixing export option

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Add this line to enable static HTML export
+  output: {
+    // If you have assets (like images) in the public folder, set this to true
+    // Otherwise, you can remove the line below
+    assetPrefix: '',
+    // This will create an export folder containing the static HTML files
+    // that can be served directly or deployed to a static hosting service.
+    exportTrailingSlash: true,
+  },
+};
+
+module.exports = nextConfig;
+
