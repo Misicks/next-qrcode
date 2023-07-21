aa2f66c21ea4d4c60cad52972f7737d0780f5388
mysicks <alo.madri@gmail.com>
2023-07-21 10:15:54 -0600

    adding just output export

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  // Add this line to enable static HTML export
-  output: {
-    // If you have assets (like images) in the public folder, set this to true
-    // Otherwise, you can remove the line below
-    // This will create an export folder containing the static HTML files
-    // that can be served directly or deployed to a static hosting service.
-    exportTrailingSlash: true,
-    output: 'export',
-  },
+  output: 'export',
 };
 
 module.exports = nextConfig;
